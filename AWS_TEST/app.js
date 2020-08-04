8829f43498bd69d739ea813d19fe347c66d36303
Eunbi Kim <kimeunb1822@gmail.com>
2020-08-04 14:10:10 -0400

    variable port

--- AWS_TEST/app.js
+++ AWS_TEST/app.js
@@ -5,7 +5,8 @@ app.get('/', (req,res)=>{
     res.send("Hello port 3001, learning AWS");
 })
 
-const port = process.env.port || 3001;
+const port = process.env.port || 3001; // if there are other env, work witht hat or use port 3001
+
 app.listen(port, ()=>{
     console.log("port 3001 listen sucess");
 });
